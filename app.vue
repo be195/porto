525c73ab7428a646d574c373609e342e67028d7b
be195 <ilya.wicopee@gmail.com>
2024-01-29 17:58:40 +0000

    remove padding on top

--- app.vue
+++ app.vue
@@ -13,36 +13,34 @@ checkAndSwitch(unref(router.currentRoute));
 
 <template>
   <div class="bg-zinc-100 text-zinc-700 dark:bg-zinc-950 dark:text-zinc-300 min-h-screen">
-    <div class="pt-8">
-      <vheader></vheader>
-      <main class="
-        px-4
-        max-w-5xl
-        m-auto
+    <vheader></vheader>
+    <main class="
+      px-4
+      max-w-5xl
+      m-auto
+    ">
+      <div class="
+        grid
+        gap-8
+        main-content
+        max-md:flex
+        max-md:flex-col-reverse
       ">
         <div class="
-          grid
-          gap-8
-          main-content
-          max-md:flex
-          max-md:flex-col-reverse
+          prose
+          lg:prose-xl
+          dark:prose-invert
+          prose-zinc
+          prose-rose
+          prose-img:rounded-2xl
+          prose-headings:text-rose-600
+          dark:prose-headings:text-rose-500
+          col-auto
         ">
-          <div class="
-            prose
-            lg:prose-xl
-            dark:prose-invert
-            prose-zinc
-            prose-rose
-            prose-img:rounded-2xl
-            prose-headings:text-rose-600
-            dark:prose-headings:text-rose-500
-            col-auto
-          ">
-            <ContentDoc />
-          </div>
-          <navigation v-if="toc && toc.links.length > 0" />
+          <ContentDoc />
         </div>
-      </main>
-    </div>
+        <navigation v-if="toc && toc.links.length > 0" />
+      </div>
+    </main>
   </div>
 </template>
